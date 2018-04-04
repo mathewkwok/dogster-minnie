2a9d779963e6a8024b38630a0d660a59194c586c
Mathew Kwok <mathew.kwok@mathewkwok-ltm1.internal.salesforce.com>
2018-04-04 12:08:20 -0700

    minnie dog

---
+++ web.archive.org/web/20140209031305if_/http_/i.kissmetrics.com/i.js
@@ -0,0 +1,21 @@
+if(typeof(_kmil) == 'function')_kmil();
+/*
+     FILE ARCHIVED ON 03:13:05 Feb 09, 2014 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 18:52:54 Apr 04, 2018.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  LoadShardBlock: 536.881 (18)
+  esindex: 0.052 (5)
+  CDXLines.iter: 283.671 (12)
+  PetaboxLoader3.datanode: 500.143 (19)
+  exclusion.robots: 1.356 (5)
+  exclusion.robots.policy: 1.162 (5)
+  RedisCDXSource: 11.527 (5)
+  PetaboxLoader3.resolve: 244.346
+  load_resource: 285.047
+*/
